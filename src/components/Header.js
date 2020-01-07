fea7346edad6abdd86153b0769a2d94032f18ba7
Bruno Novais <bruno.br001@hotmail.com>
2020-01-07 23:51:20 +0000

    header_responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import logo from "../svg/logo.svg";
+import styled from "styled-components";
 import { NavLink, Link } from "react-router-dom";
 import { Button } from "./Button";
 import Icon from "react-icons-kit";
 import { ic_keyboard_arrow_right } from "react-icons-kit/md/ic_keyboard_arrow_right";
-import styled from "styled-components";
+import { generateMedia } from "styled-media-query";
 
 class Header extends Component {
   render() {
@@ -12,13 +13,13 @@ class Header extends Component {
       <HeaderContainer className="header-container">
         <div className="header-top">
           <Link to="/">
-            <Logo className="logo" alt="logo" src={logo} />
+            <Logo className="logo" src={logo} />
           </Link>
           <NavLink to="/login" className="btn signIn-btn">
             Sign In
           </NavLink>
         </div>
-        {/* Header content */}
+        {/* Header Content */}
         <div className="header-content">
           <Title>See what's next</Title>
           <SubTitle>watch any where. cancel anytime</SubTitle>
@@ -34,7 +35,13 @@ class Header extends Component {
   }
 }
 export default Header;
-
+// Media
+const customMedia = generateMedia({
+  lgDesktop: "1350px",
+  mdDesktop: "1150px",
+  tablet: "960px",
+  smTablet: "740px"
+});
 // Logo
 const Logo = styled.img`
   width: 10rem;
@@ -44,15 +51,18 @@ const Logo = styled.img`
   left: 50%;
   transform: translate(-50%, -50%);
   margin-left: 0;
+  ${customMedia.lessThan("tablet")`
+    left: 30%;
+  `}
 `;
-
-//Header container
-const HeaderContainer = styled.div`
-  .Icon svg {
-    vertical-align: bottom !important;
-    margin-left: 1.5rem;
-  }
-
+const HeaderContainer = styled.header`
+.Icon svg {
+  vertical-align: bottom !important;
+  margin-left: 1.5rem;
+  ${customMedia.lessThan("smTablet")`
+    display: none !important;
+  `}
+}
   .signIn-btn {
     right: 0;
     margin: 1.125rem 3% 0;
@@ -65,19 +75,21 @@ const HeaderContainer = styled.div`
     position: absolute;
     translate: transform(-50%, -50%);
     cursor: pointer;
-    transition: background 0.2s ease-in;
+    transition: background 0.2s ease-in; 
     &:hover {
-      background: var(--main-red-hover);
+      background: #d30913;
     }
+    ${customMedia.lessThan("smTablet")`
+      margin-top: 1.25rem;
+      right: 5%;
+    `}
   }
-
-  //Header Top
+  // Header Top
   .header-top {
     position: relative;
     height: 10rem;
     z-index: 2;
   }
-
   // Header Content
   .header-content {
     width: 65%
@@ -89,22 +101,39 @@ const HeaderContainer = styled.div`
     text-align: center;
     flex-direction: column;
     z-index: 2;
-  }
-
-  // Lg Main
+    ${customMedia.lessThan("smTablet")`
+      grid-template-rows: repeat(3, 60px);
+      margin-top: 8rem;
+    `}
+    // Lg Main
     .main-offer-btn {
+      ${customMedia.lessThan("lgDesktop")`
+      margin: 0 33%;
+      font-size: 1.5rem;
+    `}
+    
+      ${customMedia.lessThan("mdDesktop")`
+      margin: 0 25%;
+      font-size: 1.5rem;
+      `}
+    
+      ${customMedia.lessThan("tablet")`
+        margin: 0 20%;
+        font-size: 1.3rem;
+      `}
     }
-
+  }
 `;
-
 // Main Title
 const Title = styled.h1`
   margin: 0 0 1.2rem;
   font-size: 5rem;
   font-weight: 700;
   line-height: 1.1em;
+  ${customMedia.lessThan("tablet")`
+    font-size: 2.6rem;
+  `}
 `;
-
 // SubTitle
 const SubTitle = styled.h2`
   font-weight: 400;
@@ -112,4 +141,8 @@ const SubTitle = styled.h2`
   line-height: 1.25em;
   margin: 0 0 1.875rem;
   text-transform: uppercase;
+  ${customMedia.lessThan("smTablet")`
+   font-size: 1.4rem;
+   margin: 0;
+  `}
 `;
