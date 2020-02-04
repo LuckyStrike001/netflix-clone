465cb537af97405b1d7e8c0b4650ecc04d390910
Bruno Novais <bruno.br001@hotmail.com>
2020-02-04 01:10:43 +0000

    choose_plan-component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import { Switch, Route } from "react-router-dom";
 
 import Main from "./pages/index";
+import ChoosePlan from "./pages/ChoosePlan";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Main} />
         <Route path="/login" component={Login} />
+        <Route path="/choose-plan" component={ChoosePlan} />
       </Switch>
     </div>
   );
